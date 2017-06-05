e2735f420c9356cd1323b02b7fb9033f8b01248e
Ashish Kapoor <ashish.kapoor@navitas.com>
2017-06-05 22:13:01 +0530

    added navigator (deprecated).

---
+++ components/ColorInfo.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+import {
+    Text,
+    StyleSheet,
+    View,
+    TouchableHighlight
+} from 'react-native'
+
+import ColorTools from 'color'
+
+const ColorInfo = ({ backgroundColor, onSelect=f=>f }) => {
+    const color = ColorTools(backgroundColor).negate()
+    return (
+        <TouchableHighlight style= { [styles.container, {backgroundColor} ]}
+        onPress={onSelect}>
+            <Text style={[styles.text, {color}]}> {backgroundColor} </Text>
+        </TouchableHighlight>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 40,
+        margin:10
+    }
+})
+
+export default ColorInfo
--- index.ios.js
+++ index.ios.js
@@ -9,6 +9,34 @@ import {
   AppRegistry
 } from 'react-native'
 
-import ColorList from './components/ColorList'
+import {
+  Navigator
+} from 'react-native-deprecated-custom-components'
 
-AppRegistry.registerComponent('ColorList', () => ColorList)
+import ColorList from './components/ColorList'
+import ColorInfo from './components/ColorInfo'
+
+class App extends Component {
+  render() {
+    return (
+      <Navigator 
+        initialRoute={{ name: 'Color List'}}
+          renderScene={(route, navigator) => {
+            switch (route.name) {
+              case 'Color Info':
+                return <ColorInfo backgroundColor={route.color}/>
+              default :
+                return <ColorList onColorSelected={
+                  (color) => navigator.push({
+                    name: 'Color Info',
+                    color
+                  })
+                }/> 
+            }
+          }}
+      />
+    )
+  }
+}
+
+AppRegistry.registerComponent('ColorList', () => App)
